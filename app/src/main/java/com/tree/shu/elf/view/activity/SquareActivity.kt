4b2e14645957d5bb3e6a43b843d3ee228e3a17b6
Tree <1246634075@qq.com>
2019-06-10 23:33:55 +0800

    临时保存提交

---
+++ app/src/main/java/com/tree/shu/elf/view/activity/SquareActivity.kt
@@ -0,0 +1,13 @@
+package com.tree.shu.elf.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tree.shu.elf.R
+
+class SquareActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_square)
+    }
+}
