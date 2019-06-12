bcb18f168f7526d176da122fd2a94edc32ca8c2e
Tree <1246634075@qq.com>
2019-06-12 14:51:48 +0800

    添加注释

--- app/src/main/java/com/tree/shu/elf/view/activity/SquareActivity.kt
+++ app/src/main/java/com/tree/shu/elf/view/activity/SquareActivity.kt
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.tree.shu.elf.R
 
+/**
+ * Created by Tree on 2019/6/11 17:11
+ */
+
 class SquareActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
